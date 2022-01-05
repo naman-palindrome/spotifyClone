6a5974a0f5b7dd27a0ab8658afd6995c9c596447
naman-palindrome <namanahuja80@gmail.com>
2022-01-05 20:36:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
+
+import image1 from  "./1.png"
+import image2 from "./2.PNG"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+     
+     <div className="header_image">
+       <img 
+                 src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"/>
+
+       </div>
+
+     
+
     </div>
   );
 }
